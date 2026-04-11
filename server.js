589441c1122131279f6e826daa5da4658b7a9cc2
Toska <toska.brovina@student.uni-pr.edu>
2026-04-11 12:40:33 +0200

    UDP

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
-const net = require("net");
-const fileCommands = require("./fileCommands");
+const dgram = require("dgram");
+const fs = require("fs");
 
-const server = net.createServer((socket) => {
-    console.log("Client connected");
+const server = dgram.createSocket("udp4");
 
-    socket.on("data", (data) => {
-        const command = data.toString().trim();
-        console.log("COMMAND:", command);
+let loggedUsers = {};
 
-        if (command === "list") {
-            console.log("Executing LIST...");
-            fileCommands.list(socket);
-        } else {
-            socket.write("Unknown command\n");
+server.on("message", (msg, rinfo) => {
+    const text = msg.toString().trim();
+    console.log("MESSAGE:", text);
+
+  
+    try {
+        const data = JSON.parse(text);
+
+        if (data.type === "AUTH") {
+            loggedUsers[rinfo.address] = true;
+            server.send("AUTH OK", rinfo.port, rinfo.address);
+            return;
         }
-    });
+    } catch {}
 
-    socket.on("error", (err) => {
-        console.log("Socket error:", err.message);
-    });
+    if (!loggedUsers[rinfo.address]) {
+        server.send("Please login first", rinfo.port, rinfo.address);
+        return;
+    }
 
-    socket.on("end", () => {
-        console.log("Client disconnected");
-    });
+
+    if (text === "list") {
+        fs.readdir(".", (err, files) => {
+            if (err) {
+                server.send("Error reading directory", rinfo.port, rinfo.address);
+                return;
+            }
+
+            const result = "Files:\n" + files.join("\n");
+            server.send(result, rinfo.port, rinfo.address);
+        });
+    } else {
+        server.send("Unknown command", rinfo.port, rinfo.address);
+    }
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.bind(3000, () => {
+    console.log("UDP Server running on port 3000");
 });
-
-//test kk
