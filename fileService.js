e530ecce1c153ba66f3a2e1cbbf5b2600237b89e
Sumeja <sumeja.ibrahimi@student.uni-pr.edu>
2026-04-15 15:09:30 +0200

    Krijimi i fileService

---
+++ fileService.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const { SHARED_FOLDER } = require("./config");
+
+if (!fs.existsSync(SHARED_FOLDER)) {
+  fs.mkdirSync(SHARED_FOLDER, { recursive: true });
+}
+
+function listFiles() {
+  const files = fs.readdirSync(SHARED_FOLDER);
+  return files.join("\n") || "(s ka asnjë file)";
+}
+
+function readFileContent(filename) {
+  const filePath = path.join(SHARED_FOLDER, filename);
+  if (!fs.existsSync(filePath)) {
+    return `ERROR: File ${filename} nuk ekziston`;
+  }
+  const data = fs.readFileSync(filePath, "utf8");
+  return data;
+}
+
+function saveUploadedFile(filename, base64Data) {
+  const filePath = path.join(SHARED_FOLDER, filename);
+  const buffer = Buffer.from(base64Data, "base64");
+  fs.writeFileSync(filePath, buffer);
+  return `OK: File ${filename} u ruajt në server`;
+}
+
+function getFileForDownload(filename) {
+  const filePath = path.join(SHARED_FOLDER, filename);
+  if (!fs.existsSync(filePath)) {
+    return { error: `ERROR: File ${filename} nuk ekziston` };
+  }
+  const buffer = fs.readFileSync(filePath);
+  const base64 = buffer.toString("base64");
+  return { filename, base64 };
+}
